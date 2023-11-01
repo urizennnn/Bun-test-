f3bfa0f61ad844a7c46bb95da9ac021c3dff9350
urizennnn <igamerryt@gmail.com>
2023-11-01 01:57:21 +0100

    Added timestamps

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import {testing,plugin} from './controller/test2'
 // const routes = new Routes()
 const elysia = Elysia
 const app = new elysia().get('/db-try',({body})=>{
-   const message:string = 'This is a test'
+   const message:string = 'This is a Bun test'
   
-return db(message)
+return  db(message)
 
 
 }).get('/test',()=>'Workingx').route('db','/testing',()=>{
